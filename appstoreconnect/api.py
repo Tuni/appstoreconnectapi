014c915160494a446c4adb40fc439817adb25f68
Fehmi TOUMI <fehmi.toumi@gmail.com>
2026-05-07 12:28:35 +0200

    Include territories in app availability create payload

--- appstoreconnect/api.py
+++ appstoreconnect/api.py
@@ -948,6 +948,10 @@ class Api:
 				'type': 'territoryAvailabilities'
 			})
 			included.append({
+				'id': territory_id,
+				'type': 'territories'
+			})
+			included.append({
 				'id': local_id,
 				'type': 'territoryAvailabilities',
 				'attributes': {
